f98dbcd945682208a037ab74602f696857b6d662
qwan0064 <62644761+qwan0064@users.noreply.github.com>
2020-06-14 13:42:07 +0800

    image-fetch-function.py
    image fetch lambda function

---
+++ image-fetch-function.py
@@ -0,0 +1,27 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('images')
+
+
+def lambda_handler(event, context):
+    objects = []
+    if event['httpMethod'] == 'POST':
+        data = json.loads(event['body'])
+        objects = data['objects']
+
+    records = table.scan()
+    response = {}
+    response['links'] = []
+    for i in records['Items']:
+        if set(objects) <= set(i['tags']):
+            response['links'].append(i['url'])
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps(response)
+    }
